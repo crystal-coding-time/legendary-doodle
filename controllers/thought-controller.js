c9087941ceef34caa570680b787c907a59c85b6f
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-03-25 10:34:46 -0400

    fixed spelling mistake causing error

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -82,30 +82,59 @@ const thoughtController = {
       .catch((err) => res.json(err));
   },
 
-  // Delete thought
-  deleteThought({ params }, res) {
-    // Find a thought by its _id field and delete it
-    Thought.findOneAndDelete({ _id: params.id })
-      .then((dbThoughtData) => {
-        if (!dbThoughtData) {
-            res.status(404).json({ message: "No thought with this id" });
-            return;
-          }
-          res.json(dbThoughtData);
-        })
-        .catch((err) => res.json(err));
-    },
-  
-    // Delete reaction
-    removeReaction({ params }, res) {
-      Thought.findOneAndUpdate(
-        { _id: params.thoughtId },
-        { $pull: { reactions: { reactionId: params.reactionId } } },
+ // delete Thought
+ deleteThought({ params }, res) {
+  Thought.findOneAndDelete({ _id: params.id })
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "No thought with this id!" });
+      }
+
+      // remove thought id from user's `thoughts` field
+      return User.findOneAndUpdate(
+        { thoughts: params.id },
+        { $pull: { thoughts: params.id } }, //$pull removes from an existing values that match a specified condition.
         { new: true }
-      )
-        .then((dbThoughtData) => res.json(dbThoughtData))
-        .catch((err) => res.json(err));
-    },
-  };
-  
-  module.exports = thoughtController;
+      );
+    })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        return res
+          .status(404)
+          .json({ message: "Thought created but no user with this id!" });
+      }
+      res.json({ message: "Thought successfully deleted!" });
+    })
+    .catch((err) => res.json(err));
+},
+
+// add reaction
+addReaction({ params, body }, res) {
+  Thought.findOneAndUpdate(
+    { _id: params.thoughtId },
+    { $addToSet: { reactions: body } },
+    { new: true, runValidators: true }
+  )
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "No thought with this id" });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch((err) => res.json(err));
+},
+
+// delete reaction
+removeReaction({ params }, res) {
+  Thought.findOneAndUpdate(
+    { _id: params.thoughtId },
+    { $pull: { reactions: { reactionId: params.reactionId } } },
+    { new: true }
+  )
+    .then((dbThoughtData) => res.json(dbThoughtData))
+    .catch((err) => res.json(err));
+},
+};
+
+module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {
-  getAllThought,
+  getAllThoughts,
   getThoughtById,
   createThought,
   updateThought,
@@ -10,7 +10,7 @@ const {
 } = require("../../controllers/thought-controller");
 
 // api/thoughts
-router.route("/").get(getAllThought).post(createThought);
+router.route("/").get(getAllThoughts).post(createThought);
 
 // function getAllThought(req, res) {
 //   // code to retrieve all thoughts
